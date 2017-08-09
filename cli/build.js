9eecdef0dfb920a308b7f1df53c3e09db87c3a49
lianruhe <lianruhe2013@gmail.com>
2017-08-09 11:52:36 +0800

    [A] update mocha test

---
+++ cli/build.js
@@ -0,0 +1,61 @@
+var fs = require('fs')
+var rollup = require('rollup')
+var uglify = require('uglify-js')
+var buble = require('rollup-plugin-buble')
+var version = process.env.VERSION || require('../package.json').version
+
+var banner ='/**/'
+  // '/*!\n' +
+  // ' * react-pure-render-component v' + version + '\n' +
+  // ' * (c) ' + new Date().getFullYear() + ' lianruhe\n' +
+  // ' * Released under the MIT License.\n' +
+  // ' */'
+
+// cjs
+rollup.rollup({
+  entry: 'index.js',
+  plugins: [
+    buble()
+  ]
+})
+.then(function (bundle) {
+  var code = bundle.generate({
+    format: 'cjs',
+    banner
+  }).code
+  return write('dist/index.js', code).then(function () {
+    return code
+  })
+})
+.then(function (code) {
+  var minified = banner + '\n' + uglify.minify(code, {
+    fromString: true,
+    output: {
+      ascii_only: true
+    }
+  }).code
+  return write('dist/index.min.js', minified)
+})
+.catch(logError)
+
+function write (dest, code) {
+  return new Promise(function (resolve, reject) {
+    fs.writeFile(dest, code, function (err) {
+      if (err) return reject(err)
+      console.log(blue(dest) + ' ' + getSize(code))
+      resolve()
+    })
+  })
+}
+
+function getSize (code) {
+  return (code.length / 1024).toFixed(2) + 'kb'
+}
+
+function logError (e) {
+  console.log(e)
+}
+
+function blue (str) {
+  return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m'
+}
